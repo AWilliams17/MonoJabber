87af7eb1c7b1f8d808bd7d9da828e7792f410f28
Austin Williams <awilliams17412@gmail.com>
2019-04-13 04:14:26 -0500

    Solution file added

---
+++ MonoJabber/MonoJabber/MonoJabber.cpp
@@ -0,0 +1,9 @@
+// MonoJabber.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+}
