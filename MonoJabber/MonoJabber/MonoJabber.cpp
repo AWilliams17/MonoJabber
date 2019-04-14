25c8034e4d092586c991520d4d234e58cbfda8c2
Austin Williams <awilliams17412@gmail.com>
2019-04-14 01:59:39 -0500

    Planning + .gitignore

--- MonoJabber/MonoJabber/MonoJabber.cpp
+++ MonoJabber/MonoJabber/MonoJabber.cpp
@@ -1,9 +1,22 @@
 // MonoJabber.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//		-Args List-
+// arg 1: Target Application Name
+// arg 2: Loader DLL Path
+// arg 3: Loader Namespace
+// arg 4: Loader Classname
+// arg 5: Loader Methodname
+// If any of these args are either
+//   A: Invalid
+//		-or-
+//   B: Not supplied
+// Then any valid/passed arguments should be preserved and
+// the program should ask the user to supply the rest.
+// Additionally, do a sanity check to make sure 'MJL.dll' is present & correct.
+//	(Might instead be better to just embed MJL.dll inside the program itself and deploy it that way)
 
 #include "pch.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-
+	
 }
