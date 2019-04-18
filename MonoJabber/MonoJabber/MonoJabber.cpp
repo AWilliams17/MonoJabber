323f5874e3a40b5846d8bf48918427b69120dbd9
Austin Williams <awilliams17412@gmail.com>
2019-04-17 23:25:55 -0500

    Added MonoLoaderDLL project files

--- MonoJabber/MonoJabber/MonoJabber.cpp
+++ MonoJabber/MonoJabber/MonoJabber.cpp
@@ -13,9 +13,20 @@
 // the program should ask the user to supply the rest.
 // Additionally, do a sanity check to make sure 'MJL.dll' is present & correct.
 //	(Might instead be better to just embed MJL.dll inside the program itself and deploy it that way)
+// Also determine if the bitness of the target matches the bitness of the injector.
 
 #include "pch.h"
 #include <iostream>
+
+
+bool DLLExists() {
+	return false;
+}
+
+bool TargetUsesMono() {
+	return false;
+}
+
 
 int main(int argc, char* argv[]) {
 	
